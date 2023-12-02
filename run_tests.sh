c04e7782bf85565bcf602d571cd1441d7afcc5a7
Anthony Massaad <massaadanthony@hotmail.com>
2023-12-02 10:59:45 -0500

    typo

--- run_tests.sh
+++ run_tests.sh
@@ -6,6 +6,6 @@ elif command -v python &> /dev/null; then
     echo "Using Python 2"
     python -m unittest src/tests/test_*.py
 else
-    echo "Error: Python not found. Please install Python version 3.8 or > to run the tests."
+    echo "Error: Python not found. Please install Python version 3.8 or greater to run the tests."
     exit 1
 fi
